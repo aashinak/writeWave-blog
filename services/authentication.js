17be0a348a2123fd5d857614c617b342a1dc2ad4
AASHIN A K <aashin9605@gmail.com>
2024-06-08 16:24:14 +0530

    create authentication services (JWT)

---
+++ services/authentication.js
@@ -0,0 +1,24 @@
+const JWT = require('jsonwebtoken')
+require('dotenv').config()
+
+const createTokenForUser = (user) => {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        profileImageUrl: user.profileImageUrl,
+        role: user.role
+    }
+
+    const token = JWT.sign(payload, process.env.JWT_SECRET)
+    return token
+}
+
+const validateToken = (token) => {
+    const payload = JWT.verify(token, process.env.JWT_SECRET)
+    return payload
+}
+
+module.exports = {
+    createTokenForUser,
+    validateToken
+}
