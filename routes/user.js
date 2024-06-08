12996ef3def16418a4e71d9bbd101e49c014329d
AASHIN A K <aashin9605@gmail.com>
2024-06-08 16:54:12 +0530

    create user route

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const { Router } = require("express")
+const USER = require("../models/user")
+
+const router = Router()
+
+router.get("/signin", (req,res) => {
+    return res.render("signin")
+})
+
+router.get("/signup", (req,res) => {
+    return res.render("signup")
+})
+
+router.post("/signup", async (req,res) => {
+    const { fullName, email, password } = req.body
+    await USER.create({
+        fullName,
+        email,
+        password
+    })
+
+    return res.redirect("/")
+    
+})
+
+module.exports = router
